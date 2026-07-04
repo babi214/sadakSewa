41d0c95704687d35732aaee5bcbf878590e8b3c4
trevonacid <bimalbhandari563@gmail.com>
2026-07-04 17:30:58 +0545

    fix: remove JSX comment inside JS expression causing babel parse error

--- client/src/pages/public/Home.jsx
+++ client/src/pages/public/Home.jsx
@@ -268,7 +268,6 @@ export default function Home() {
       </section>
 
       {!isAuthenticated && (
-        {/* CTA Section */}
         <section className="py-20 sm:py-28">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <motion.div
